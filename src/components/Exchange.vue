01b76a60303618360c09ec8344b88347fffd3f58
alant <alan.htang@gmail.com>
2018-06-20 23:34:29 -0700

    added select in exchange, fixed the hamburgur menu issue by including the material-icon css

--- src/components/Exchange.vue
+++ src/components/Exchange.vue
@@ -1,26 +1,40 @@
 <template>
   <v-container grid-list-md text-xs-center>
+    <v-layout row wrap justify-center>
+      <v-flex xs3>
+        <v-subheader>Select the coin to trade: </v-subheader>
+      </v-flex>
+      <v-flex xs3>
+        <v-select :items="items" v-model="e1" label="Select" single-line></v-select>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap justify-center>
+      <v-flex xs5>
+        <v-alert v-model="alert" type="info" dismissible>
+          This is what NRT is about
+        </v-alert>
+      </v-flex>
+    </v-layout>
     <v-layout row wrap>
       <v-flex xs6>
         <v-card justify-start>
           <v-card-text class="px-0">Buy</v-card-text>
-            <v-layout row>
-              <v-flex offset-xs3 xs3>
-                <v-subheader>price:
-                </v-subheader>
-              </v-flex>
-              <v-flex xs6>
-                <v-subheader>
-                  {{buyPrice}}
-                </v-subheader>
-              </v-flex>
-            </v-layout>
-            <v-layout row>
-               <v-flex>
-               <v-text-field v-model="buyNumbers" type="number"
-                          placeholder="0"/>
-               </v-flex>
-            </v-layout>
+          <v-layout row>
+            <v-flex offset-xs3 xs3>
+              <v-subheader>price:
+              </v-subheader>
+            </v-flex>
+            <v-flex xs6>
+              <v-subheader>
+                {{buyPrice}}
+              </v-subheader>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex>
+              <v-text-field v-model="buyNumbers" type="number" placeholder="0" />
+            </v-flex>
+          </v-layout>
           <v-btn color="green" v-on:click="buyToken">
             buy
           </v-btn>
@@ -29,23 +43,22 @@
       <v-flex xs6>
         <v-card justify-start>
           <v-card-text class="px-0">Sell</v-card-text>
-            <v-layout row>
-              <v-flex offset-xs3 xs3>
-                <v-subheader>price:
-                </v-subheader>
-              </v-flex>
-              <v-flex xs6>
-                <v-subheader>
-                  {{sellPrice}}
-                </v-subheader>
-              </v-flex>
-            </v-layout>
-            <v-layout row>
-               <v-flex>
-               <v-text-field v-model="sellNumbers" type="number"
-                          placeholder="0"/>
-               </v-flex>
-            </v-layout>
+          <v-layout row>
+            <v-flex offset-xs3 xs3>
+              <v-subheader>price:
+              </v-subheader>
+            </v-flex>
+            <v-flex xs6>
+              <v-subheader>
+                {{sellPrice}}
+              </v-subheader>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex>
+              <v-text-field v-model="sellNumbers" type="number" placeholder="0" />
+            </v-flex>
+          </v-layout>
           <v-btn color="red" v-on:click="sellToken">
             sell
           </v-btn>
@@ -56,7 +69,6 @@
 </template>
 
 <script>
-
 export default {
   name: 'Exchange',
   data() {
@@ -64,7 +76,10 @@ export default {
       sellNumbers: 0,
       buyNumbers: 0,
       sellPrice: 0.12,
-      buyPrice: 0.13
+      buyPrice: 0.13,
+      e1: null,
+      items: [{ text: 'NRT' }, { text: 'USDnt' }],
+      alert: true
     };
   },
   methods: {
