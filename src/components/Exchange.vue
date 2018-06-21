688b10984d578efa2374f4aebdec2331e0a60a71
alant <alan.htang@gmail.com>
2018-06-21 07:35:22 -0700

    added dropdown, redeployed contract because of contract changes

--- src/components/Exchange.vue
+++ src/components/Exchange.vue
@@ -26,7 +26,7 @@
             </v-flex>
             <v-flex xs6>
               <v-subheader>
-                {{buyPrice}}
+                <v-text-field v-model="currentPrice" type="number" placeholder="currentPrice" />
               </v-subheader>
             </v-flex>
           </v-layout>
@@ -69,29 +69,74 @@
 </template>
 
 <script>
+const HttpRequest = require('nebulas').HttpRequest;
+const Wallet = require('nebulas');
+// const NebPay = require('nebpay');
+
 export default {
   name: 'Exchange',
   data() {
     return {
+      currentPrice: 0,
       sellNumbers: 0,
       buyNumbers: 0,
       sellPrice: 0.12,
       buyPrice: 0.13,
-      e1: null,
-      items: [{ text: 'NRT' }, { text: 'RMBnt' }],
+      e1: 'NRT',
+      items: [{ text: 'NRT', value: 'NRT' }, { text: 'RMBnt', value: 'RMBnt' }],
       alert: true
     };
   },
   methods: {
     buyToken() {
-      console.log(`Buy ${this.buyNumbers} tokens is called.`);
+      console.log(
+        `Buy ${this.buyNumbers} tokens is called. ${this.e1} is selected`
+      );
     },
     sellToken() {
       console.log(`Sell ${this.sellNumbers} tokens is called.`);
+    },
+    getPrice() {
+      console.log('=> exchange getPrice')
+      const testAccount1 = new Wallet.Account(
+        '2b779296ab0ee991a73ecc61319afff8352d171b0a8778ef623911f65d7bf5b4'
+      );
+      const neb = new Wallet.Neb();
+      neb.setRequest(new HttpRequest('https://testnet.nebulas.io'));
+      const toAddress = 'n1tqSiUCEqr7B5E61hJ74M1GavFE9yPH9rx';
+      const gasLimit = 200000;
+      const gasPrice = 1000000;
+
+      const call = {
+        function: 'getCurrentPrice',
+        args: '[]'
+      };
+      neb.api
+        .call(
+          testAccount1.getAddressString(),
+          toAddress,
+          0,
+          '0',
+          gasPrice,
+          gasLimit,
+          call
+        )
+        .then((resp) => {
+          if (resp.execute_err.length > 0) {
+            throw new Error(resp.execute_err);
+          }
+          const result = JSON.parse(resp.result) / (10 ** 18);
+          if (!result) {
+            throw new Error('访问合约API出错');
+          }
+
+          this.currentPrice = result;
+        });
     }
   },
   beforeMount() {
-    console.log('Before mounting.');
+    console.log('=> exchange beforeMOunt');
+    this.getPrice();
   }
 };
 </script>
